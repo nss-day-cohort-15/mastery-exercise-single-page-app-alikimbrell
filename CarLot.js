d96c2840bb33ff51061432953978169c9761518d
Ali Kimbrell <alexandrakimbrell@gmail.com>
2016-08-01 17:02:14 -0500

    Set-up

---
+++ CarLot.js
@@ -0,0 +1,21 @@
+// Technical Req #1. Create one global variable (e.g. CarLot) and use the IIFE pattern to augment it two times in separate JavaScript files.
+// Technical Req #2.  The first IIFE should add a public function (e.g. loadInventory) that loads the inventory.json file and stores the inventory in a private variable. It should also expose a public getter to read the array of cars (e.g. getInventory).
+
+
+var CarLot = (function () {
+  var inventory = [];
+
+  return {
+    getInventory: function () {
+
+    },
+    loadInventory: function (callback) {
+      var inventoryLoader = new XMLHttpRequest();
+
+      inventoryLoader.addEventListener("load", function () {
+
+      });
+    }
+  };
+
+})();
