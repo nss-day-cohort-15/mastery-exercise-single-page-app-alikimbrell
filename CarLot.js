65b8a3d0f8958f7fbfad1d2c0d554152e0f34c8c
Ali Kimbrell <alexandrakimbrell@gmail.com>
2016-08-05 14:13:10 -0500

    Requiremensts 1-2.

--- CarLot.js
+++ CarLot.js
@@ -1,21 +1,37 @@
 // Technical Req #1. Create one global variable (e.g. CarLot) and use the IIFE pattern to augment it two times in separate JavaScript files.
+
 // Technical Req #2.  The first IIFE should add a public function (e.g. loadInventory) that loads the inventory.json file and stores the inventory in a private variable. It should also expose a public getter to read the array of cars (e.g. getInventory).
 
 
-var CarLot = (function () {
+var CarLot = (function() {
   var inventory = [];
 
   return {
-    getInventory: function () {
-
+    getInventory: function(callback) {
+      return inventory
+      
     },
-    loadInventory: function (callback) {
+    loadInventory: function(callback) {
       var inventoryLoader = new XMLHttpRequest();
-
-      inventoryLoader.addEventListener("load", function () {
-
-      });
+      inventoryLoader.open('GET', 'inventory.json')
+      inventoryLoader.addEventListener("load", function (event)
+      {
+        inventory = JSON.parse(event.target.responseText).cars
+        callback(inventory)
+      })
+      inventoryLoader.send();
     }
-  };
+  }
 
-})();
+})(CarLot || {});
+
+
+console.log("loaded");
+
+
+
+
+
+
+
+
--- Styling.js
+++ Styling.js
@@ -1,3 +1,7 @@
-// Technical Req. #4The final IIFE should augment the object with two more functions. One function resets the border thickness and background color for each car element back to the original values. The other function changes the thickness of the border of a car element, and changes its background color. The function must accept two arguments.
+// Technical Req. #4. The final IIFE should augment the object with two more functions. One function resets the border thickness and background color for each car element back to the original values. The other function changes the thickness of the border of a car element, and changes its background color. The function must accept two arguments.
   // A car DOM element that was clicked on.
   // A color name.
+
+function styling (ph1, ph2) {
+
+}
